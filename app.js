e6ca21e23348ea706efef1864d972e722d8dde7b
SJCMN <sam.clavette@gmail.com>
2021-02-25 23:24:21 -0600

    Outline for To Do List app

---
+++ app.js
@@ -0,0 +1,19 @@
+// prompt user for input
+let input = prompt('Add new to do');
+
+while (input) {
+    prompt('Add new to do')
+}
+
+// Array to save to do's
+const toDoList = []
+
+// Add item to list
+
+// Delete item from array
+
+// Quit app
+
+// Re order list by alphabetical
+
+
